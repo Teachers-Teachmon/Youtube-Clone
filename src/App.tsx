6a91bc4cccb1a01eb2fbb9110b350a73c5c7456b
ketarubot <robin09v@naver.com>
2026-01-26 19:29:43 +0900

    feat(#2) :: 전역 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-function App() {
+import "@/styles/global.css";
 
+function App() {
   return (
     <>
       <h1>유튜브 멋지게 만들어봐용</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
