cd7f30fe1030e19569433288dc760033511685eb
ketarubot <robin09v@naver.com>
2026-01-31 23:27:20 +0900

    feat(#12) :: 검색버튼 추가 및 스타일 변경

--- src/components/ui/search-bar/index.tsx
+++ src/components/ui/search-bar/index.tsx
@@ -2,9 +2,17 @@ import * as S from "./style";
 
 function SearchBar() {
   return (
-    <S.Button>
-      <img src="/assets/mic.svg" width={28} />
-    </S.Button>
+    <S.Base>
+      <S.InputBox>
+        <S.Input placeholder="검색" />
+        <S.SearchButton>
+          <img src="/assets/search.svg" width={28} />
+        </S.SearchButton>
+      </S.InputBox>
+      <S.Button>
+        <img src="/assets/mic.svg" width={28} />
+      </S.Button>
+    </S.Base>
   );
 }
 
--- src/components/ui/search-bar/style.ts
+++ src/components/ui/search-bar/style.ts
@@ -1,11 +1,47 @@
 import styled from "@emotion/styled";
 
-export const InputBox = styled.input``;
+export const Base = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  height: 40px;
+  gap: 2vw;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  max-width: 650px;
+  width: 40vw;
+  border: 1px solid #00000034;
+  border-radius: 20px;
+  box-sizing: border-box;
+  height: 100%;
+`;
+
+export const Input = styled.input`
+  flex: 2 1 90%;
+  border-radius: 20px 0 0 20px;
+  border: none;
+  padding: 12px 16px;
+  font-size: 16px;
+  box-sizing: border-box;
+  height: 100%;
+`;
+
+export const SearchButton = styled.button`
+  border-left: 1px solid #00000034;
+  background-color: rgba(0, 0, 0, 0.05);
+  flex: 1 1 64px;
+  border-radius: 0 20px 20px 0;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
 
 export const Button = styled.button`
   background-color: #ebebeb;
-  width: 40px;
-  height: 40px;
+  aspect-ratio: 1 / 1;
+  height: 100%;
   border-radius: 20px;
   padding: 0;
   margin: 0;
