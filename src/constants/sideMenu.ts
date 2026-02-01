0efb6839403e88bfa69a7940f72d62398d4ca144
ketarubot <robin09v@naver.com>
2026-02-01 19:17:23 +0900

    refactor(#12) :: 상수 따로 분리

---
+++ src/constants/sideMenu.ts
@@ -0,0 +1,15 @@
+export const shortcuts = [
+  { icon: "/assets/home.svg", script: "홈" },
+  { icon: "/assets/shorts.svg", script: "Shorts" },
+  { icon: "/assets/subscriptions.svg", script: "구독" },
+  { icon: "/assets/accountBlack.svg", script: "내 페이지" },
+];
+export const openShortcuts = [
+  [
+    { icon: "/assets/home.svg", script: "홈" },
+    { icon: "/assets/shorts.svg", script: "Shorts" },
+    { icon: "/assets/subscriptions.svg", script: "구독" },
+    { icon: "/assets/accountBlack.svg", script: "내 페이지" },
+    { icon: "/assets/history.svg", script: "기록" },
+  ],
+];
